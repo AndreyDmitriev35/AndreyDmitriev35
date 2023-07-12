3d94229413a45e3c15c9e43f6fdc81be44ba6a1f
AndreyDmitriev35 <dn19sm@mail.ru>
2023-07-12 13:59:52 +0300

    Доделано

--- Practicum/Pr_21_9_1.py
+++ Practicum/Pr_21_9_1.py
@@ -8,4 +8,6 @@ class Rectangle:
     def __str__(self):
         return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.height}'
 
-print(Rectangle(5,10,50,100))
+
+rect = Rectangle(5,10,50,100)
+print(rect)
